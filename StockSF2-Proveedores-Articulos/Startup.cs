6a85a0a13286f33ff09f0a283d347554c70d6b79
Sebastián Pintos <sebastianpintosgm@gmail.com>
2022-08-28 13:05:23 -0300

    Add project files.

---
+++ StockSF2-Proveedores-Articulos/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace StockSF2_Proveedores_Articulos
+{
+    //se configuran sertvicios y middlewares
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting(); //lo agregamos, no viene de program
+
+            app.UseAuthorization();
+
+            //app.MapControllers(); no va, lo sacamos
+
+            app.UseEndpoints(endpoints =>   //agregamos este
+            {
+                endpoints.MapControllers();
+            });
+        }
+       
+    }
+}
